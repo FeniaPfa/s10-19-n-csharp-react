9999471d44957e85cbab2453a100f408440aa113
FeniaPfa <fa.aguilarpfa@gmail.com>
2024-09-18 21:45:40 -0300

    fix: routes, add linter

--- frontend/src/layout/Contain/Contain.jsx
+++ frontend/src/layout/Contain/Contain.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import { useDispatch } from 'react-redux'
-import { getCategories } from '../../features/category/categorySlice'
+import React, { useEffect } from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { useDispatch } from 'react-redux';
+import { getCategories } from '../../features/category/categorySlice';
 
 const Contain = ({ children }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCategories())
-  }, [])
+    dispatch(getCategories());
+  }, []);
 
   return (
-    <>
+    <main className="main overflow-hidden box-border flex flex-col gap-8">
       <Header />
       {children}
       <Footer />
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default Contain
+export default Contain;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,51 +1,46 @@
-import { Route, Routes } from 'react-router-dom'
-import React from 'react'
-import NotFound from '../pages/notFound/NotFound'
-import Home from '../pages/home/Home'
-import Restaurant from '../pages/restaurant/Restaurant'
-import Contact from '../pages/Contact/Contact'
-import Register from '../pages/Register/Register'
-import ListOfProducts from '../pages/ListOfProducts/ListOfProducts'
-import RestaurantID from '../pages/restaurant/RestaurantID'
-import ProfilePage from '../components/profile/profile'
-import LoginUser from '../pages/Login/Login'
-import Detail from '../components/Detail/Detail'
-import CheckOut from '../pages/CheckOut/CheckOut'
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import NotFound from '../pages/notFound/NotFound';
+import Home from '../pages/home/Home';
+import Restaurant from '../pages/restaurant/Restaurant';
+import Contact from '../pages/Contact/Contact';
+import Register from '../pages/Register/Register';
+import ListOfProducts from '../pages/ListOfProducts/ListOfProducts';
+import RestaurantID from '../pages/restaurant/RestaurantID';
+import ProfilePage from '../components/profile/profile';
+import LoginUser from '../pages/Login/Login';
+import Detail from '../components/Detail/Detail';
+import CheckOut from '../pages/CheckOut/CheckOut';
 
-import { useSelector } from 'react-redux'
-import { MealsByCategory } from '../pages/MealsByCategory/MealsByCategory'
-import Categories from '../pages/Menus/Categories'
+import { useSelector } from 'react-redux';
+import { MealsByCategory } from '../pages/MealsByCategory/MealsByCategory';
+import Categories from '../pages/Menus/Categories';
 
 const AppRoutes = () => {
-  const { logged } = useSelector(state => state?.auth2)
+  const { logged } = useSelector((state) => state?.auth2);
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/home' element={<Home />} />
-      {/* <Route path='/home' element={<Home />} /> */}
-      <Route path='/restaurant' element={<Restaurant />} />
-      <Route path='/restaurant/:id' element={<RestaurantID />} />
-      <Route path='/category' element={<Categories />} />
-      <Route path='/category/:id' element={<MealsByCategory />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/register' element={<Register />} />
-      {
-        logged === true &&
-          <Route path='/profile' element={<ProfilePage />} />
-      }
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurant" element={<Restaurant />} />
+      <Route path="/restaurant/:id" element={<RestaurantID />} />
+      <Route path="/category" element={<Categories />} />
+      <Route path="/category/:id" element={<MealsByCategory />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/register" element={<Register />} />
+      {logged === true && <Route path="/profile" element={<ProfilePage />} />}
 
-      <Route path='/detalle/:id' element={<Detail />} />
+      <Route path="/detalle/:id" element={<Detail />} />
 
       {/* <Route path='/detalle:id/:owner' element={<Detail />} /> */}
 
       {/* defecto page error */}
-      <Route path='/menu' element={<ListOfProducts />} />
-      <Route path='*' element={<NotFound />} />
-      <Route path='/login' element={<LoginUser />} />
-      <Route path='/car' element={<CheckOut />} />
+      <Route path="/menu" element={<ListOfProducts />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/login" element={<LoginUser />} />
+      <Route path="/car" element={<CheckOut />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
